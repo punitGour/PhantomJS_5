b8ae5ba8756db0bc3a0a0ce52d450e5d2bf44ae7
Poorvi Dharwad <poorvid@cybage.com>
2020-07-09 15:24:33 +0530

    commit-1 , Junit phantomjs

---
+++ src/main/java/PhantomJS_5/PhantomJS_5/App.java
@@ -0,0 +1,13 @@
+package PhantomJS_5.PhantomJS_5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/PhantomJS_5/PhantomJS_5/FirstTest.java
@@ -0,0 +1,41 @@
+package PhantomJS_5.PhantomJS_5;
+
+import java.net.MalformedURLException;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import junit.framework.Assert;
+
+public class FirstTest {
+	
+	WebDriver driver;
+	@Test
+	public void checkTitle() throws MalformedURLException {
+		
+		//System.setProperty("phantomjs.binary.path", "lib/phantomjs-2.1.1-windows/bin/phantomjs.exe");
+		// driver = new PhantomJSDriver();
+		
+		//Capabilities caps = new DesiredCapabilities();
+	//caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "lib/phantomjs-2.1.1-windows/bin/phantomjs.exe");
+	//	driver = new PhantomJSDriver(caps);
+		
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"lib/phantomjs");
+		WebDriver driver = new  PhantomJSDriver(caps);
+		
+		driver.get("http://newtours.demoaut.com");
+		//driver.get(baseUrl);
+
+		System.out.println(driver.getTitle());
+		Assert.assertEquals("Welcome: Mercury Tours", driver.getTitle());
+
+		driver.quit();
+	}
+}
+
+
+
